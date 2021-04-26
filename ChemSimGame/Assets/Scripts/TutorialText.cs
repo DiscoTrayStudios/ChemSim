d26ee8153f4a5e1076e27a9cab35b02284be8c89
ianc02 <77699025+ianc02@users.noreply.github.com>
2021-04-26 02:12:06 -0500

    Basic working Tutorial

--- ChemSimGame/Assets/Scripts/TutorialText.cs
+++ ChemSimGame/Assets/Scripts/TutorialText.cs
@@ -14,10 +14,13 @@ public class TutorialText : MonoBehaviour
     public GameObject backToMenuButton;
     public GameObject H2ODropdown;
     public GameObject CO2Button;
-    public GameObject allChildren;
+    public GameObject tryReactionButton;
+    public GameObject allReactants;
+    public GameObject allOptions;
 
     private int clicks;
     private bool co2Clicked;
+    private bool h2oclicked;
     private bool certainSelection;
     // Start is called before the first frame update
     void Start()
@@ -25,6 +28,7 @@ public class TutorialText : MonoBehaviour
         main.text = "Welcome to the ChemSim Tutorial! We're going to go over some of the basics so you can get more comfortable with the system. Please click to continue!";
         clicks = 0;
         co2Clicked = false;
+        h2oclicked = false;
     }
 
     // Update is called once per frame
@@ -37,6 +41,15 @@ public class TutorialText : MonoBehaviour
         }
         if (certainSelection)
         {
+            if (clicks == 4)
+            {
+                Disable();
+                H2ODropdown.GetComponent<TMP_Dropdown>().interactable = true;
+                H2ODropdown.GetComponent<TMP_Dropdown>().onValueChanged.AddListener(delegate
+                {
+                    H2OPressed();
+                });
+            }
             if (clicks == 5)
             {
                 Disable();
@@ -44,11 +57,18 @@ public class TutorialText : MonoBehaviour
                 CO2Button.GetComponent<Button>().onClick.AddListener(CO2Pressed);
                 
             }
+            if (clicks == 6)
+            {
+                Disable();
+                tryReactionButton.GetComponent<Button>().interactable = true;
+                tryReactionButton.GetComponent<Button>().onClick.AddListener(ReactionPressed);
+            }
         }
     }
 
     private void click(int clicks)
     {
+        Debug.Log(clicks);
         if (clicks == 1)
         {
             main.text = "";
@@ -66,28 +86,31 @@ public class TutorialText : MonoBehaviour
         }
         else if (clicks == 4)
         {
+            certainSelection = true;
             buttons.text = "";
             reactants.text = "Lets try making a reaction! Try selecting the liquid state of H2O.";
-            //Code to make sure that happens
+            H2ODropdown.GetComponent<Image>().color = Color.yellow;
         }
         else if (clicks == 5)
         {
+            H2ODropdown.GetComponent<Image>().color = Color.white;
             certainSelection = true;
             reactants.text = "";
             reactants.text = "Great! Now press on the CO2 button.";
             CO2Button.GetComponent<Image>().color = Color.yellow;
 
-            //Code to make sure that happens
         }
         else if (clicks == 6)
         {
             CO2Button.GetComponent<Image>().color = Color.white;
             reactants.text = "";
             buttons.text = "Lets see what happens when we press the Try Reaction button";
-            //Code to make sure that happens
+            tryReactionButton.GetComponent<Image>().color = Color.yellow;
+            certainSelection = true;
         }
         else if (clicks == 7)
         {
+            tryReactionButton.GetComponent<Image>().color = Color.white;
             buttons.text = "";
             main.text = "Good Job! You just made your first reaction! Before we are done, there is another important piece of information to help you use this tool.";
         }
@@ -95,12 +118,11 @@ public class TutorialText : MonoBehaviour
         {
             main.text = "";
             reactants.text = "You can have up to two different kinds of reactants, but up to 3 molecules of one reactant. To get multiple molecules of a reactant, just press on the button or drop down option again.";
-            //Code to make sure that happens
         }
         else if (clicks == 9)
         {
             reactants.text = "";
-            main.text = "Perfect! You can stay in here and play with the system more to get more comfortable or go back to the main menu anytime!";
+            main.text = "You can stay in here and play with the system more to get more comfortable or go back to the main menu anytime!";
         }
         else
         {
@@ -124,21 +146,28 @@ public class TutorialText : MonoBehaviour
 
     public void Disable()
     {
-        foreach(Transform child in allChildren.transform)
+        foreach(Transform child in allReactants.transform)
         {
             if (child.gameObject.GetComponent<Button>() != null)
             {
                 child.gameObject.GetComponent<Button>().interactable = false;
             }
-            if (child.gameObject.GetComponent<Dropdown>() != null)
+            if (child.gameObject.GetComponent<TMP_Dropdown>() != null)
             {
-                child.gameObject.GetComponent<Dropdown>().interactable = false;
+                child.gameObject.GetComponent<TMP_Dropdown>().interactable = false;
+            }
+        }
+        foreach (Transform child in allOptions.transform)
+        {
+            if (child.gameObject.GetComponent<Button>() != null)
+            {
+                child.gameObject.GetComponent<Button>().interactable = false;
             }
         }
     }
     public void Enable()
     {
-        foreach (Transform child in allChildren.transform)
+        foreach (Transform child in allReactants.transform)
         {
             if (child.gameObject.GetComponent<Button>() != null)
             {
@@ -149,13 +178,37 @@ public class TutorialText : MonoBehaviour
                 child.gameObject.GetComponent<Dropdown>().interactable = true;
             }
         }
+        foreach (Transform child in allOptions.transform)
+        {
+            if (child.gameObject.GetComponent<Button>() != null)
+            {
+                child.gameObject.GetComponent<Button>().interactable = true;
+            }
+        }
     }
 
     private void CO2Pressed()
     {
         Enable();
         co2Clicked = true;
-        clicks += 1;
+        certainSelection = false;
+        clicks =6;
+        click(clicks);
+    }
+    private void H2OPressed()
+    {
+        Enable();
+        h2oclicked = true;
+        certainSelection = false;
+        clicks = 5;
+        click(clicks);
+    }
+
+    private void ReactionPressed()
+    {
+        Enable();
+        certainSelection = false;
+        clicks = 7;
         click(clicks);
     }
 }
