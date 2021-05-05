47a2f26e3e7b718ae931b56b35faa45d083c10e9
ianc02 <77699025+ianc02@users.noreply.github.com>
2021-05-04 20:26:54 -0500

    last questions should be right now

--- ChemSimGame/Assets/Scripts/ReactionManagement/ReactionManager.cs
+++ ChemSimGame/Assets/Scripts/ReactionManagement/ReactionManager.cs
@@ -54,7 +54,7 @@ public class ReactionManager : MonoBehaviour
 
     public TextMeshProUGUI targetDhText;
 
-    private double[] targetDHs = new double[5] { -30.5, -5.0, -92.2, -100, -111.6};
+    private double[] targetDHs = new double[5] { -30.5, -5.0, -92.2, -100, -200.6};
     private int currentTarget = 0;
 
     private bool productPresent;
